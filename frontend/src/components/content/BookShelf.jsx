a400d6dc0a5cb66a5e6316acaebe69ba8b6baef9
Mohammed Bahageel <Mohmmed.Bahageel@DSAH.SA>
2025-06-02 12:56:33 +0300

    shelf

--- frontend/src/components/content/BookShelf.jsx
+++ frontend/src/components/content/BookShelf.jsx
@@ -3,57 +3,51 @@ import React from "react";
 import "../../styles/content/BookShelf.css";
 
 // Book image paths are in /public/images/
-// PDF paths are assumed to be in /public/pdfs/
 const books = [
   {
     id: 1,
     title: "Practical Manual Of In Vitro Fertilization",
-    image: "/images/manual.png",
+    image: "/images/manual.png", // make sure this exists in public/images
     url: "https://heyzine.com/flip-book/a1a2f21a60.html",
-    pdfUrl: "/pdfs/manual.pdf", // Example PDF path
   },
   {
     id: 2,
     title: "Fertility Cryopreservation",
-    image: "/images/cryopreservation.png",
+    image: "/images/cryopreservation.png", // make sure this exists in public/images
     url: "https://heyzine.com/flip-book/9eaacb692e.html",
-    pdfUrl: "/pdfs/cryopreservation.pdf", // Example PDF path
   },
-  {
+   {
     id: 3,
     title: "The Ethics Of IVF",
-    image: "/images/ethics.png",
+    image: "/images/ethics.png", // make sure this exists in public/images
     url: "https://heyzine.com/flip-book/b296d506ea.html",
-    pdfUrl: "/pdfs/ivf_ethics.pdf", // Example PDF path
   },
-  {
+   {
     id: 4,
     title: "Boston Handbook Of Infertility",
-    image: "/images/bostonIvf.png",
+    image: "/images/bostonIvf.png", // make sure this exists in public/images
     url: "https://heyzine.com/flip-book/d90118581a.html",
-    pdfUrl: "/pdfs/ivf_handbook.pdf", // Example PDF path
   },
   {
     id: 5,
     title: "How to prepare endometrium ",
-    image: "/images/endo.png",
+    image: "/images/endo.png", // make sure this exists in public/images
     url: "https://heyzine.com/flip-book/30f4394f2a.html",
-    pdfUrl: "/pdfs/endo.pdf", // Example PDF path
-  },
+  }
+  ,
   {
     id: 6,
     title: "How to Prepare Eggs for IVF",
-    image: "/images/egg.png",
+    image: "/images/egg.png", // make sure this exists in public/images
     url: "https://heyzine.com/flip-book/1f97ea61c2.html",
-    pdfUrl: "/pdfs/eggs_preparation.pdf", // Example PDF path
-  },
+  }
+  ,
   {
     id: 7,
     title: "Handbook For Infertility",
-    image: "/images/ivf.png",
+    image: "/images/ivf.png", // make sure this exists in public/images
     url: "https://heyzine.com/flip-book/b6b460f174.html",
-    pdfUrl: "/pdfs/boston.pdf", // Example PDF path
-  },
+  }
 ];
 
 const BookShelf = ({ onSelectBook, selectedBookUrl }) => {
@@ -63,10 +57,8 @@ const BookShelf = ({ onSelectBook, selectedBookUrl }) => {
       {books.map((book) => (
         <div
           key={book.id}
-          className={`book-item ${
-            selectedBookUrl === book.url ? "selected" : ""
-          }`}
-          onClick={() => onSelectBook(book)} // Pass the whole book object
+          className={`book-item ${selectedBookUrl === book.url ? "selected" : ""}`}
+          onClick={() => onSelectBook(book.url)}
         >
           <img src={book.image} alt={book.title} className="book-image" />
           <div className="book-title">{book.title}</div>
